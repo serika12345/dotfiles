d15f2caad2c13d33a4e764349258da54ddc35595
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-07-25 11:56:49 +0900

    software keyboard

--- surface/desktop.nix
+++ surface/desktop.nix
@@ -48,95 +48,96 @@ in
   documentation.nixos.enable = false;
 
   programs.dconf.enable = true;
-  programs.dconf.profiles.user.databases = [
-    {
-      settings = {
-        "org/gnome/desktop/a11y/applications" = {
-          screen-keyboard-enabled = true;
-        };
+  # Write these values to the user's dconf database. System profile values are
+  # only defaults and lose to values already stored by a previous GNOME login.
+  home-manager.users.masato.xdg.dataFile."gnome-shell/extensions/keyboard-toggle@SAH046.github.io".source =
+    "${pkgs.gnomeExtensions.keyboard-toggle}/share/gnome-shell/extensions/keyboard-toggle@SAH046.github.io";
+  home-manager.users.masato.dconf.settings = {
+    "org/gnome/desktop/a11y/applications" = {
+      screen-keyboard-enabled = true;
+    };
 
-        "org/gnome/desktop/interface" = {
-          accent-color = "blue";
-          clock-format = "24h";
-          clock-show-date = true;
-          color-scheme = "prefer-dark";
-          scaling-factor = mkUint32 2;
-          show-battery-percentage = true;
-          toolkit-accessibility = true;
-        };
+    "org/gnome/desktop/interface" = {
+      accent-color = "blue";
+      clock-format = "24h";
+      clock-show-date = true;
+      color-scheme = "prefer-dark";
+      scaling-factor = mkUint32 2;
+      show-battery-percentage = true;
+      toolkit-accessibility = true;
+    };
 
-        "org/gnome/desktop/input-sources" = {
-          current = mkUint32 1;
-          sources = [
-            (mkTuple [
-              "xkb"
-              "jp"
-            ])
-            (mkTuple [
-              "ibus"
-              "mozc-jp"
-            ])
-          ];
-          mru-sources = [
-            (mkTuple [
-              "ibus"
-              "mozc-jp"
-            ])
-            (mkTuple [
-              "xkb"
-              "jp"
-            ])
-          ];
-          per-window = false;
-          show-all-sources = false;
-          xkb-model = "pc105+inet";
-          xkb-options = [ "ctrl:nocaps" ];
-        };
+    "org/gnome/desktop/input-sources" = {
+      current = mkUint32 1;
+      sources = [
+        (mkTuple [
+          "xkb"
+          "jp"
+        ])
+        (mkTuple [
+          "ibus"
+          "mozc-jp"
+        ])
+      ];
+      mru-sources = [
+        (mkTuple [
+          "ibus"
+          "mozc-jp"
+        ])
+        (mkTuple [
+          "xkb"
+          "jp"
+        ])
+      ];
+      per-window = false;
+      show-all-sources = false;
+      xkb-model = "pc105+inet";
+      xkb-options = [ "ctrl:nocaps" ];
+    };
 
-        "org/gnome/settings-daemon/peripherals/touchscreen" = {
-          orientation-lock = false;
-        };
+    "org/gnome/settings-daemon/peripherals/touchscreen" = {
+      orientation-lock = false;
+    };
 
-        "org/gnome/settings-daemon/plugins/power" = {
-          # Settings > Power > Dim Screen
-          idle-dim = false;
-          # Settings > Power > Automatic Screen Brightness
-          ambient-enabled = false;
-          # Settings > Power > Automatic Power Saver
-          power-saver-profile-on-low-battery = false;
-          # Settings > Power > Automatic Suspend > When Plugged In
-          sleep-inactive-ac-type = "nothing";
-        };
+    "org/gnome/settings-daemon/plugins/power" = {
+      # Settings > Power > Dim Screen
+      idle-dim = false;
+      # Settings > Power > Automatic Screen Brightness
+      ambient-enabled = false;
+      # Settings > Power > Automatic Power Saver
+      power-saver-profile-on-low-battery = false;
+      # Settings > Power > Automatic Suspend > When Plugged In
+      sleep-inactive-ac-type = "nothing";
+    };
 
-        "org/gnome/shell" = {
-          disable-user-extensions = false;
-          enabled-extensions = [
-            "appindicatorsupport@rgcjonas.gmail.com"
-            "dash-to-dock@micxgx.gmail.com"
-            "no-overview@fthx"
-          ];
-          favorite-apps = [
-            "firefox.desktop"
-            "org.kde.krita.desktop"
-            "org.gnome.Nautilus.desktop"
-            "org.gnome.Console.desktop"
-            "org.gnome.Settings.desktop"
-          ];
-        };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "dash-to-dock@micxgx.gmail.com"
+        "keyboard-toggle@SAH046.github.io"
+        "no-overview@fthx"
+      ];
+      favorite-apps = [
+        "firefox.desktop"
+        "org.kde.krita.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Console.desktop"
+        "org.gnome.Settings.desktop"
+      ];
+    };
 
-        "org/gnome/shell/extensions/dash-to-dock" = {
-          autohide = false;
-          background-opacity = 0.8;
-          dash-max-icon-size = mkInt32 48;
-          dock-fixed = true;
-          dock-position = "LEFT";
-          extend-height = true;
-          intellihide = false;
-          show-apps-at-top = false;
-        };
-      };
-    }
-  ];
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      autohide = false;
+      background-opacity = 0.8;
+      dash-max-icon-size = mkInt32 48;
+      dock-fixed = true;
+      dock-position = "LEFT";
+      extend-height = true;
+      intellihide = false;
+      show-apps-at-top = false;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     adwaita-icon-theme
